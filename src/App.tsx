3967b1c9b512cb812ea3567661428506d77410b4
VITA-R9 <jhj9144@gmail.com>
2023-11-19 17:19:43 +0900

    feat home, profile routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Layout from "./components/layout.tsx";
+import Home from "./routes/home.tsx";
+import Profile from "./routes/profile.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout/>,
+        children: [
+            {
+                path: "",
+                element: <Home/>,
+            },
+            {
+                path: "profile",
+                element: <Profile/>,
+            },
+        ]
+    }
+])
+
 function App() {
-    return (<></>)
+    return <><RouterProvider router={router}/></>
 }
 
 export default App
