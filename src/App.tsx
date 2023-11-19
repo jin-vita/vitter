1d9e88a4c2ef71badad0a09e3933748368c53c32
VITA-R9 <jhj9144@gmail.com>
2023-11-19 17:50:43 +0900

    feat login, create-account routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Layout from "./components/layout.tsx";
 import Home from "./routes/home.tsx";
 import Profile from "./routes/profile.tsx";
+import Login from "./routes/login.tsx";
+import CreateAccount from "./routes/create-account.tsx";
+import {createGlobalStyle} from "styled-components";
+import reset from "styled-reset";
 
 const router = createBrowserRouter([
     {
@@ -17,11 +21,38 @@ const router = createBrowserRouter([
                 element: <Profile/>,
             },
         ]
-    }
+    },
+    {
+        path: "/login",
+        element: <Login/>
+    },
+    {
+        path: "/create-account",
+        element: <CreateAccount/>
+    },
 ])
 
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: black;
+    color: white;
+    font-family: 'system-ui', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
 function App() {
-    return <><RouterProvider router={router}/></>
+    return (
+        <>
+            <GlobalStyles/>
+            <RouterProvider router={router}/>
+        </>
+    );
 }
 
 export default App
